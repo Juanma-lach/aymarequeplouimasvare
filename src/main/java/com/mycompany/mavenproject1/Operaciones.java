22775767c1bf0947a64299c190ec3207ea0740f7
al038888 <rafaaznar@gmail.com>
2015-01-19 15:13:27 +0100

    Merge origin/master
    Conflicts:
    	database.sql

---
+++ src/main/java/com/mycompany/mavenproject1/Operaciones.java
@@ -0,0 +1,19 @@
+package com.mycompany.mavenproject1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author al038888
+ */
+public class Operaciones {
+    Integer num1 = 2;
+    Integer num2 = 4;
+    
+    public Integer multiplicacion (){
+        return num1 * num2;
+    }
+}
---
+++ src/test/java/com/mycompany/mavenproject1/OperacionesTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author al038888
+ */
+public class OperacionesTest {
+    
+    public OperacionesTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of multiplicacion method, of class Operaciones.
+     */
+    @Test
+    public void testMultiplicacion() {
+        System.out.println("multiplicacion");
+        Operaciones instance = new Operaciones();
+        Integer expResult = 8;
+        Integer result = instance.multiplicacion();
+        assertEquals(expResult, result);
+    }
+    
+}
